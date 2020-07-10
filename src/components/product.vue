5b5f50253f15dc45bea820e29ee44e23364f18fc
tugce3434 <tugcenurdaglar@gmail.com>
2020-07-10 14:28:53 +0300

    component yapıldı

---
+++ src/components/product.vue
@@ -0,0 +1,129 @@
+<template>
+    <div class="product">
+        <div class="row p-2">
+
+            <div class="col-md-4">
+                <div class="product-image">
+                    <img :src="image" :alt="altText"></div>
+                <h5 v-show="inventory ">In Stock</h5>
+
+            </div>
+
+            <div class="col-md-4">
+                <div class="product-info">
+
+                    <div class="row">
+                        <ul>
+                            <li v-for="detail in details" :key="detail">{{ detail }}</li>
+                        </ul>
+                    </div>
+
+
+                    <div class="row p-3">
+                        <div class="color-box "
+                             v-for="(variant,index) in variants"
+                             :key="variant.variantId"
+                             :style="{backgroundColor: variant.variantColor }"
+                             @mouseover="updateProduct(index)"
+                        ></div>
+                    </div>
+
+
+                </div>
+
+
+            </div>
+
+            <div class="col-md-4">
+                <div class="cart ">
+                    <button v-on:click="addToCart"
+                            :disabled="!inStock"
+                            :class="{ disabledButton:!inStock }"
+                    >Add to Cart
+
+                    </button>
+
+                    <p>Cart {{ cart }}</p>
+                </div>
+            </div>
+
+
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "product",
+        props: {
+            premium: {
+                type: Boolean,
+                required: true
+            }
+        },
+        data(){
+            return{
+                product: 'Socks',
+                brand:'HM',
+                selectedVariant:0,
+
+                details: ["80% cotton", "20% polyester", "Gender-neutral"],
+                variants: [
+                    {
+                        variantId: 2234,
+                        variantColor: "green",
+                        variantImage: "./images/vmSocks-green-onWhite.jpg",
+                        variantQuantity:10,
+                    },
+                    {
+                        variantId: 2235,
+                        variantColor: "blue",
+                        variantImage: "./images/vmSocks-blue-onWhite.jpg",
+                        variantQuantity:0,
+
+
+                    }
+
+                ],
+                cart: 0,
+
+
+
+            }
+        },
+        methods:{
+            addToCart() {
+                this.cart += 1;
+            },
+            updateProduct(index) {
+                this.selectedVariant = index
+                console.log(index)
+            }
+
+        },
+        computed:{
+            image(){
+                return this.variants[this.selectedVariant].variantImage
+
+            },
+            inStock(){
+                return this.variants[this.selectedVariant].variantQuantity
+
+            },
+            shipping(){
+                if (this.premium){
+                    return "Free"
+                }else {
+                    return  2.99
+                }
+            }
+
+        }
+
+
+    }
+</script>
+
+<style scoped>
+
+</style>
